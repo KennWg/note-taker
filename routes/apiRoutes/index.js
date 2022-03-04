53e470e224229e6b05936efa9b60e6357c0589b3
KW <kenneth_sl_wong@msn.com>
2022-03-04 16:28:21 -0500

    added nanoid to give unique IDs to notes (also fixed the viewing of notes)

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const {nanoid} = require('nanoid');
 const notes = require('../../db/db.json');
 const { newNote, validateNote } = require('../../lib/dbFunctions');
 
@@ -10,6 +11,7 @@ router.post("/notes", (req,res) => {
     if(!validateNote(req.body)){
         res.status(400).send('Please enter both a title and text for the note');
     } else {
+        req.body.id = nanoid();
         const note = newNote(req.body, notes);
         res.json(note);
     }
