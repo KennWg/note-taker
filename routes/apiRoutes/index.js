80078e712cfc43a72872adc4254f1dfd5e36c52b
KW <kenneth_sl_wong@msn.com>
2022-03-04 15:38:36 -0500

    added functions for creating notes and api routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,3 +1,18 @@
 const router = require('express').Router();
+const notes = require('../../db/db.json');
+const { newNote, validateNote } = require('../../lib/dbFunctions');
+
+router.get("/api/notes", (req,res) => {
+    res.json(notes);
+})
+
+router.post("/api/notes", (req,res) => {
+    if(!validateNote(req.body)){
+        res.status(400).send('Please enter both a title and text for the note');
+    } else {
+        const note = newNote(req.body, notes);
+        res.json(note);
+    }
+});
 
 module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,14 +3,14 @@ const path = require('path');
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
-  });
+});
 
-  router.get('/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
-  });
+});
 
-  router.get('*', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
-  });
+});
 
 module.exports = router;
